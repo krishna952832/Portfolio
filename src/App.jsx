49b8a05013c3d69a4bb01b7369728bd8e23dedd4
krishna kumar <106673407+krishna952832@users.noreply.github.com>
2024-06-30 07:43:45 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import PortFolio from "./components/PortFolio";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import { Toaster } from "react-hot-toast";
+import './index.css';
+function App() {
+  return (
+    <>
+    <div id="preloader"></div>
+      <div>
+        <Navbar />
+        <Home />
+        <About />
+        <PortFolio />
+        <Experience />
+        <Contact />
+        <Footer />
+      </div>
+      <Toaster />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello! I’m Krishna Kumar, final year graduate in software development.
+          Passionate about coding, problem-solving, and creating efficient
+          software solutions. Excited to contribute my skills and learn from
+          experienced professional.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          B.Tech University Institute of Technology, RGPV BHOPAL 2021-25
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Technical Skills: ReactJs, NodeJs, Express, MongoDb, SQL,API
+          integration, c++, JAVA, Python <br />
+          Soft Skills: communication skills, listening skills, time management,
+          problem-solving
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          Assessor (Garbage free city) 3 June 2022 - 30 June 2022 <br /> IQVIA <br />
+          Nawagarh, Madhya Pradesh <br /> • Monitor compliance with waste management
+          regulations and guidelines within the community. <br />• Collect data on
+          waste generation, recycling rates, and diversion efforts to evaluate
+          the effectiveness of waste reduction initiatives. <br /> • Prepare regular
+          reports and presentations on key performance indicators and
+          recommendations for improvement.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+         EXTRA-CURRICULAR ACTIVITIES
+        </h1>
+        <span>
+        Devoted my time and energy to volunteering at the prestigious KHELO INDIA Games, supporting the nation’s
+        thriving sports culture with passion and dedication
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative software solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
